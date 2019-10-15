63b5a570e727626c9f1d0418a6476afbe7b67d5b
sanderomeo <56474421+sanderomeo@users.noreply.github.com>
2019-10-15 21:57:21 +0300

    Code2
    Test2

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Codetest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "ROMY";
+            int charaterAge = 25;
+
+            Console.WriteLine(" there was 35 a nice guy named ROMY ");
+            Console.WriteLine(" there was 35 a nice guy named " + characterName);
+            Console.WriteLine(" there was " + charaterAge + " a nice guy");
+
+            characterName = "TOM";
+            Console.WriteLine(" there was 35 a nice guy named " + characterName);
+            Console.WriteLine(" there was a nice guy named again");
+
+        }
+    }
+}
